31aadb6a60f1f212c2c00b3d66152411f23f0a81
pierre <pierre.sreckcep@gmail.com>
2023-01-07 17:44:28 +0100

    update

--- BotPortier.py
+++ BotPortier.py
@@ -1,48 +1,83 @@
 import discord as d
 from discord.ext import commands
-from discord.activity import CustomActivity
-import asyncio
-import sys
-from gpiozero import Button
+from discord.activity import Game
+from gpiozero import Button, Device
+from gpiozero.pins.mock import MockFactory
 import discord.ext.tasks as tasks
+import logging
+import asyncio
 
-from NouveauBotPortier.constants import DoorStatus
+
+from constants import DoorStatus
 
 class BotPortier(commands.Bot):
-    doorStatus : str
-    doorStatus_isOverriden: bool
     button : Button
     configuration: dict
+
+    doorStatus : d.activity.Game
+    overridenDoorStatus: d.activity.Game
+
     status : d.Status
+    overridenStatus : d.Status
 
-    def __init__(self, conf: dict) -> None:
-        super().__init__(command_prefix=conf["prefix"], intents=conf["intents"])
-        self.configuration = conf
+    override: bool
+
+    door: bool
+    def __init__(self, configuration: dict) -> None:
+        super().__init__(command_prefix=configuration["prefix"], intents=d.Intents.all())
+        #simulate the pi
+        Device.pin_factory = MockFactory()
+        self.configuration = configuration
         self.button = Button(4)
-        self.checkButton.start()
+        self.doorStatus = Game(self.configuration["ACTIVITY_STRING"]["CLOSED"])
+        self.status = d.Status.do_not_disturb
+        self.override = False
+        self.door = True
+        logging.basicConfig(filename=configuration["logfile"])
         
     
-    @tasks.loop(seconds=30)
+    @tasks.loop(seconds=10)
     async def checkButton(self) -> None:
-        """checks the door state and changes the presence if it is not currently overriden"""
-        if not self.doorStatus_isOverriden:
-            if self.button.is_active:
-                self.doorStatus = DoorStatus.OPEN
-                self.status = d.Status.online
-            else:
-                self.doorStatus = DoorStatus.CLOSED
-                self.status = d.Status.do_not_disturb
-        
-        await self.change_presence(activity=CustomActivity(DoorStatus.OPEN), status=self.status)
-        #send a notification inside the specified channel
-        await self.get_channel(self.configuration["notification_channel"]).send(f"Local {self.doorStatus} !")
+        """checks the door state every x seconds and changes the presence if it is not currently overriden"""
+        logging.info("checkedButton")
+        print("test")
+        #remember the precedent status to act later if it changed
+        oldDoorStatus = self.doorStatus
+
+
+        if self.door:#self.button.is_active:
+            self.doorStatus = Game(self.configuration["ACTIVITY_STRING"]["OPEN"])
+            self.status = d.Status.online
+        else:
+            self.doorStatus = Game(self.configuration["ACTIVITY_STRING"]["CLOSED"])
+            self.status = d.Status.do_not_disturb
+
+        #change the bot's presence if it's not currently overriden
+        if self.override :
+            await self.updatePresenceOverride()
+        else:
+            await self.updatePresence()
+            if oldDoorStatus != self.doorStatus:
+                channel = self.get_channel(self.configuration["notification_channel"])
+                await channel.send(self.doorStatus.name) #type:ignore
+
     
 
-    @checkButton.before_loop
-    async def before_checkDoorStatus(self) -> None:
-        print("waiting for bot...")
-        #TODO
+
+    async def overrideDisplay(self, activity: d.Game, status:d.Status):
+        """override the door state and display custom activity (usually for events)"""
+        self.override = True
+        self.overridenDoorStatus = activity
+        self.overridenStatus = status
+        await self.updatePresenceOverride()
+
+    async def updatePresence(self):
+        logging.info(f"changed bot presence to: [{self.doorStatus.name}] | [{self.status}]")
+        await self.change_presence(activity=self.doorStatus, status=self.status)
     
+    async def updatePresenceOverride(self):
+        logging.info(f"changed bot presence to: [{self.overridenDoorStatus.name}] | [{self.overridenStatus}]")
+        await self.change_presence(activity=self.overridenDoorStatus, status=self.overridenStatus)
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-import NouveauBotPortier.BotPortier as BotPortier
-import NouveauBotPortier.constants as constants
+import BotPortier as BotPortier
+import os
+import yaml
+import logging
+from discord import Game, Status
+from discord.ext.commands import Context
 
+configuration_path = os.path.abspath(os.path.dirname(__file__))
 
-configuration = constants.Configuration()
+with open(os.path.join(configuration_path, "conf.yaml.local"), "r") as stream:
+    conf = yaml.safe_load(stream)
 
 bot = BotPortier.BotPortier(configuration=conf)
+
+@bot.event
+async def on_ready():
+    print("ready")
+    bot.checkButton.start()
+
+@bot.event
+async def on_connect():
+    print("connected")
+
+@bot.command()
+async def changeDisplay(ctx : Context, arg1 : str, arg2 : str):
+    #TODO check roles
+    await bot.overrideDisplay(Game(arg1), Status(arg2))
+
+async def stopDisplay(ctx : Context):
+    bot.override = False
+
+@bot.command()
+async def changeDoorValue(ctx : Context, arg1 : str):
+    bot.door = True if arg1 == "true" else False
+
+bot.run(conf["bot_token"])
